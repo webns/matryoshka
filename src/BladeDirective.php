2baa05ed6cf7b9fa042f63805564a5de34b63266
Nikita Sklyarov <nsweb.d@gmail.com>
2018-11-13 22:20:34 +0200

    Accept additional options for cache key

--- src/BladeDirective.php
+++ src/BladeDirective.php
@@ -74,29 +74,39 @@ class BladeDirective
     /**
      * Normalize the cache key.
      *
-     * @param mixed       $item
-     * @param string|null $key
+     * @param mixed      $item
+     * @param array|null $options
      */
-    protected function normalizeKey($item, $key = null)
+    protected function normalizeKey($item, $options = null)
     {
         // If the user wants to provide their own cache
         // key, we'll opt for that.
-        if (is_string($item) || is_string($key)) {
-            return is_string($item) ? $item : $key;
+        if (is_string($item)) {
+            $key = $item;
         }
 
         // Otherwise we'll try to use the item to calculate
         // the cache key, itself.
         if (is_object($item) && method_exists($item, 'getCacheKey')) {
-            return $item->getCacheKey();
+            $key = $item->getCacheKey();
         }
 
         // If we're dealing with a collection, we'll
         // use a hashed version of items keys.
         if ($item instanceof \Illuminate\Support\Collection) {
-            return md5($item->map->getKey());
+            $key = md5($item->map->getKey());
         }
 
-        throw new Exception('Could not determine an appropriate cache key.');
+        if (!isset($key)) {
+            throw new Exception('Could not determine an appropriate cache key.');
+        }
+
+        // If additional options are passed, we'll
+        // add them to the key.
+        if (is_array($options)) {
+            $key .= implode('.', $options);
+        }
+
+        return $key;
     }
 }
